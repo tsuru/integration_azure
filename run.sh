7d805334981b9df0e7b934b60d36d8a93031d069
Cezar Sa Espinola <cezarsa@gmail.com>
2019-02-18 13:00:11 -0300

    Ensure clean up of azure resource from previous build

--- run.sh
+++ run.sh
@@ -10,12 +10,28 @@ if [[ "$AZURE_CLIENT_ID" == "" ]] ||
     exit 1
 fi
 
+function az_cleanup() {
+    sudo apt-get install apt-transport-https lsb-release software-properties-common dirmngr -y
+    AZ_REPO=$(lsb_release -cs)
+    echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
+        sudo tee /etc/apt/sources.list.d/azure-cli.list
+    sudo apt-key --keyring /etc/apt/trusted.gpg.d/Microsoft.gpg adv \
+        --keyserver packages.microsoft.com \
+        --recv-keys BC528686B50D79E339D3721CEB3E94ADBE1229CF
+    sudo apt-get update
+    sudo apt-get install azure-cli -y
+    az login --service-principal -u "$AZURE_CLIENT_ID" -p "$AZURE_CLIENT_SECRET" --tenant "$AZURE_TENANT_ID"
+    az group delete -y --name docker-machine
+}
+
 export AZURE_RESOURCE_GROUP='docker-machine'
 export AZURE_VIRTUAL_NETWORK='docker-machine-vnet'
 export AZURE_SUBNET='docker-machine'
 export AZURE_LOCATION="westus"
 export AZURE_AGENT_VM_SIZE="Standard_A4_v2"
 export AZURE_AGENT_POOL_NAME="agentpool0"
+
+which apt-get && az_cleanup
 
 set -e
 
